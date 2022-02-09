5aca0fafb392e89a59f7414bfd3803a20d77a011
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2022-02-09 16:46:34 -0300

    comit all

---
+++ Desafio8QualiV2/Data/WebContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Desafio8QualiV2.Data
+{
+    public class WebContext : DbContext
+    {
+        //public DbSet<Conta> Contas { get; set; }
+        //public DbSet<Despesa> Despesas { get; set; }
+        //public DbSet<Receita> Receitas { get; set; }
+
+        public WebContext(DbContextOptions<WebContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
